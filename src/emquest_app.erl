4cb014f84cbe1df89856ccbae1eccf56326ca7e1
Steve Roques <steve.roques@gmail.com>
2025-07-05 05:48:05 +0200

    merge with embryo

--- src/emquest_app.erl
+++ src/emquest_app.erl
@@ -4,8 +4,7 @@
 
 start(_StartType, _StartArgs) ->
     io:format("Emquest V0.1.0~n"),
-    Port = get_embox_port(),
-    
+    Port = embryo:get_port_from_env("embox_port", 8079),
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/", emquest_handler, [index]},
@@ -13,39 +12,12 @@ start(_StartType, _StartArgs) ->
             {"/static/[...]", cowboy_static, {priv_dir, emquest, "static"}}
         ]}
     ]),
-    
     {ok, _} = cowboy:start_clear(
         emquest_http,
         [{port, Port}],
         #{env => #{dispatch => Dispatch}}
     ),
-    
     emquest_sup:start_link().
 
 stop(_State) ->
     ok.
-
--spec get_embox_port() -> integer().
-get_embox_port() ->
-    case os:getenv("embox_port") of
-        false ->
-            ConfigMap = embryo:read_emergence_conf(),
-            get_port_from_config(ConfigMap);
-        Url -> 
-            try list_to_integer(Url)
-            catch _:_ -> 8079
-            end
-    end.
-
--spec get_port_from_config(map() | undefined) -> integer().
-get_port_from_config(undefined) -> 8079;
-get_port_from_config(ConfigMap) ->
-    case maps:get("embox", ConfigMap, undefined) of
-        undefined -> 8079;
-        Embox ->
-            PortStr = maps:get("port", Embox, "8079"),
-            try list_to_integer(PortStr)
-            catch _:_ -> 8079
-            end
-    end.
-
