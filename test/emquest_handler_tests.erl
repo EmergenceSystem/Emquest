ff09caa5257ab573357129a71188c941b79a7c84
Steve Roques <steve.roques@gmail.com>
2026-08-29 06:05:25 +0000

    feat(media): carry embryo media fields through normalise_item

--- test/emquest_handler_tests.erl
+++ test/emquest_handler_tests.erl
@@ -7,3 +7,23 @@ parse_stt_text_extracts_text_test() ->
 
 parse_stt_text_empty_on_garbage_test() ->
     ?assertEqual(<<>>, emquest_handler:parse_stt_text(<<"not json">>)).
+
+normalise_item_media_passthrough_test() ->
+    Raw = #{<<"properties">> => #{<<"media_type">> => <<"image">>,
+                                  <<"thumbnail">>  => <<"http://x/t.jpg">>,
+                                  <<"media_url">>  => <<"http://x/f.jpg">>,
+                                  <<"title">>      => <<"Cat">>,
+                                  <<"license">>    => <<"CC BY 2.0">>,
+                                  <<"url">>        => <<"http://x/page">>}},
+    Item = emquest_handler:normalise_item(Raw),
+    ?assertEqual(<<"image">>,        maps:get(<<"media_type">>, Item)),
+    ?assertEqual(<<"http://x/t.jpg">>, maps:get(<<"thumbnail">>, Item)),
+    ?assertEqual(<<"http://x/f.jpg">>, maps:get(<<"media_url">>, Item)),
+    ?assertEqual(<<"CC BY 2.0">>,    maps:get(<<"license">>, Item)),
+    ?assertEqual(<<"Cat">>,          maps:get(<<"label">>, Item)).
+
+normalise_item_non_media_unchanged_test() ->
+    Raw  = #{<<"properties">> => #{<<"url">> => <<"http://x">>, <<"title">> => <<"T">>}},
+    Item = emquest_handler:normalise_item(Raw),
+    ?assertEqual(false, maps:is_key(<<"media_type">>, Item)),
+    ?assertEqual(<<"http://x">>, maps:get(<<"url">>, Item)).
